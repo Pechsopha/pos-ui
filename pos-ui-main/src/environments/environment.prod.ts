d1503059d17e5e4072726e02a456ce1db343f4d1
sopha <pichsopha874@gmail.com>
2021-12-20 15:53:25 +0700

    initial commit

---
+++ pos-ui-main/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  hmr: false, 
+  token: 'cp-token', 
+
+  apiUrl      : 'http://128.199.197.254:8001/api',
+  fileUrl     : 'http://128.199.197.254:8001/',
+  url         : 'http://128.199.197.254:8001',
+
+  user:       '',
+  password:   ''
+};
---
+++ pos-ui-main/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  hmr: false, 
+  token: 'cp-token', 
+  
+  apiUrl      : 'http://127.0.0.1:8000/api',
+  fileUrl     : 'http://128.199.197.254:8001/',
+  url         : 'http://128.199.197.254:8001',
+
+  
+  user:       '',
+  password:   ''
+};
